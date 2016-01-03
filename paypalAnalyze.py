2662b8480965162219477d3d210375ac3a27d9d5
prg4hire <prg4hire@gmail.com>
2016-01-03 16:54:14 +0530

    improved/changed string messages

--- paypalAnalyze.py
+++ paypalAnalyze.py
@@ -11,6 +11,34 @@
 # My bitcoin address is 1JVM63khCnZbi36i3w2pbXWgKJ1VaRLP9o
 
 import csv, sys
+
+monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+def	addValue( currencies, currency, name, month, amount):
+	m = int(month)
+	#print "In addvalue name = ", name, " month = ", m, " amount = ", amount
+	#print currencies[currency][name]
+
+	val = 0.0
+	try:
+		val = float(currencies[currency][name][ m])
+		#print "val = ", val
+	except:
+		val = 0.0
+    
+	val = val + float( amount)
+
+	l = len(currencies[currency][name])
+	if( l < m):
+		for i in range( l, m):
+			currencies[currency][name].append(0)
+		currencies[currency][name].append( val)
+	else:
+		currencies[currency][name][m] = val
+	currencies[currency][name] [0] = currencies[currency][name] [0] + float(amount)
+	#print currencies[currency][name]
+	return currencies
+
 def usage():
     print "Usage:\n\nanalyzePayPal.py PayPalTransactionsFile.csv\n"
     print ""
@@ -38,27 +66,59 @@ for date,time, timezone, name, typ, status, currency, amount, receiptid,empty, b
     if "Payment Received" in typ or "eCheque Received" in typ:
         if "Update" in typ:    # some transactiions are updattes, such as update that echeque cleared. We ignore these.
             continue 
+        month = date[3:5]
+        #print month 
         if currency in currencies:
             if name in currencies[currency]:
-                currencies[currency][name] = currencies[currency][name] + float(amount)
+            	currencies = addValue( currencies, currency, name, month, amount)
+                #currencies[currency][name].insert( int(month) , float(amount))
             else:
-                currencies[currency][name] = float(amount)
+            	currencies[currency][name] = list()
+            	currencies = addValue( currencies, currency, name, month, amount)
         else:
             currencies[currency] = dict()
-            currencies[currency][name] = float(amount)
+            currencies[currency][name] = list()
+            currencies = addValue( currencies, currency, name, month, amount)
         
     # as of 25 dec 2015: Two types of comments. 1. "Withdrawn to: XYZ Bank ..." 2. "Withdraw funds to Bank Account"
     if typ.find( "ithdraw") != -1:
         totalWithdrawn = totalWithdrawn + float(amount[1:])
 
-print "\nClient names and total amounts sent by them:\n"
+print "\nClient names and total amounts sent by them in a specific currency:\n"
 for c in sorted( currencies, key=currencies.get):
     total = 0
-    print "%s" % ( c)
+    print "\n%s was received from following clients:\n" % ( c) # EUR/USD
+
+    months = list()
+    currtotal = 0
     for n in sorted( currencies[c], key=currencies[c].get):
-        total = total + currencies[c][n]
-        print "%30s: %s" % (n, currencies[c][n])
+        usertotal = 0
+        i = 0
+
+        for val in currencies[c][n]:
+        	#print "name = ", n , " val = ", val
+	        if i > 0: #ignore first value
+	        	usertotal = usertotal + val
+        		l = len( months)
+        		if l < i:
+        			for j in range( l, i):
+        				months.append(0.0)
+        			months[i-1] = months[i-1] +  float(val)
+        		else:
+        			months[i-1] = months[i-1] + float(val)
+        	i = i + 1
+        print "%30s: %s" % (n, usertotal)
+        currtotal = currtotal + usertotal
     print "                ----------------------"
-    print "%23s %s: %s" %( c, " total", total)
-         
+    print "%23s %s: %s\n\n" %( c, " total", currtotal)
+    
+    print c, "received for each month: \n"
+    for mname in monthNames:
+    	print "%5s" % mname, 
+    print ""
+    for m in months:
+    	print "{:5.0f}".format(m),  
+
+    print "\n--------------------------------------------------------------------------------"
+
 print "\nTotal Withdrawn in local currency = ", totalWithdrawn
