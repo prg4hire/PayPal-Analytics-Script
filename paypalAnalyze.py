86a4ac49f3e92eeec2d645a415b23514dd54a19e
Vishal <prg4hire@gmail.com>
2021-07-10 15:54:48 +0530

    Ported for python 3 and tested with latest paypal data file

--- paypalAnalyze.py
+++ paypalAnalyze.py
@@ -23,12 +23,12 @@ logger = logging.getLogger(__name__)
 
 def usage():
     """ Prints the usage message """
-    print "Usage:\n\nanalyzePayPal.py PayPalTransactionsFile.csv\n"
-    print ""
-    print "To get/create the PayPalTransactionsFile.csv, log in to your PayPal account."
-    print "Then follow Activity -> Statements -> Activity Export"
-    print "Select period, and in File Type, select Comma Delimited - All Activity"
-    print "Click Download. The file downloaded is the input file to this script, to be given as first command line argument"   
+    print( "Usage:\n\nanalyzePayPal.py PayPalTransactionsFile.csv\n")
+    print ("")
+    print ("To get/create the PayPalTransactionsFile.csv, log in to your PayPal account.")
+    print ("Then follow Activity -> Statements -> Activity Export")
+    print ("Select period, and in File Type, select Comma Delimited - All Activity")
+    print ("Click Download. The file downloaded is the input file to this script, to be given as first command line argument")
 
 def analyze(filename):
     """ This function analyzes the Paypal csv transaction file """
@@ -44,18 +44,25 @@ def analyze(filename):
     totalWithdrawn = 0
 
     try:
-        csvlines = csv.reader(open(filename, "r"))
+        csvlines = csv.reader(open(filename, "r",encoding='utf-8-sig'))
     except:
-        print "Could not open file", filename
+        print ("Could not open file", filename)
         usage()
         exit()
+    #  "Date","Time","Time zone","Name","Type","Status","Currency","Amount","Receipt ID","Balance"
 
-    for date,time, timezone, name, typ, status, currency, amount, receiptid,empty, balance in csvlines:
+    #for line in csvlines:
+    #    print(line)
+
+    for date,time, timezone,      name,  typ,   status,  currency,  amount,  receiptid, balance,  in csvlines:
         amount = amount.replace(",", "") # change numbers from 1,123 to 1123
+        date = date.replace('\"', '')
 
-        if "Payment Received" in typ or "eCheque Received" in typ:
+
+        if "General Payment" in typ or "Mobile Payment" in typ or "Payment Received" in typ or "eCheque Received" in typ:
             if "Update" in typ:    # some transactiions are updattes, such as update that echeque cleared. We ignore these.
                 continue 
+            date.strip()
             month = date[3:5]
             if currency in currencies:
                 if name not in currencies[currency]:
@@ -73,14 +80,14 @@ def analyze(filename):
     return currencies, totalWithdrawn
 
 def printText(currencies, totalWithdrawn):
-    print "\nClient names and total amounts sent by them in a specific currency:\n"
-    for c in sorted( currencies, key=currencies.get):
+    print ("\nClient names and total amounts sent by them in a specific currency:\n")
+    for c in currencies:
         total = 0
-        print "\n%s was received from following clients:\n" % ( c) # EUR/USD
+        print ("\n%s was received from following clients:\n" % ( c)) # EUR/USD
 
         months = [0]* 12
         currtotal = 0
-        for n in sorted( currencies[c], key=currencies[c].get):
+        for n in currencies[c]:
             usertotal = 0
             i = 0
 
@@ -90,19 +97,19 @@ def printText(currencies, totalWithdrawn):
                     usertotal = usertotal + val
                     months[i-1] = months[i-1] + float(val)
                 i = i + 1
-            print "%30s: %s" % (n, usertotal)
+            print ("%30s: %s" % (n, usertotal))
             currtotal = currtotal + usertotal
-        print "                ----------------------"
-        print "%23s %s: %s\n\n" %( c, " total", currtotal)
+        print ("                ----------------------")
+        print ("%23s %s: %s\n\n" %( c, " total", currtotal))
         
-        print c, "received for each month: \n"
+        print (c, "received for each month: \n")
         for mname in monthNames:
-            print "%5s" % mname, 
-        print ""
+            print ("%5s" % mname , end =" ")
+        print ("")
         for m in months:
-            print "{:5.0f}".format(m),  
-        print "\n--------------------------------------------------------------------------------"
-    print "\nTotal Withdrawn in local currency = ", totalWithdrawn
+            print ("{:5.0f}".format(m), end =" ")
+        print ("\n--------------------------------------------------------------------------------")
+    print ("\nTotal Withdrawn in local currency = ", totalWithdrawn)
 
 if __name__ == "__main__":
     logger.debug('Start script')
